32cd3c7520d2a6f679f4613b94e2b086c156d7ac
JasonSaber <jsaber19@windwardschool.org>
2018-11-14 18:43:15 -0800

    initial

---
+++ MailServer.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+public class MailServer extends LinkedList<Message> {
+    private TreeSet<Actor> subscribers = new TreeSet<Actor>();
+
+
+    public void signUp(Actor actor){
+        subscribers.add(actor);
+    }
+
+    public void dispatch(Message msg){
+        if (msg.getRecipient().equals(null)) {
+            for (Actor a : subscribers){
+                a.receive(msg);
+            }
+        }
+
+        else {
+            msg.getRecipient().receive(msg);
+        }
+    }
+
+}
